0ad25a51b71985e7b00ff3e5a8fa334652ec440d
Hleb <hlebchaba@gmail.com>
2020-07-22 21:01:45 +0300

    Initial Commit

---
+++ FishingFishing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FishingFishing
+//
+//  Created by mac on 22.07.2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
