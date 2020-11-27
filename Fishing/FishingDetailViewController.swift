35b8e4d42ff3ea9b1de3c6a2e3fd3083b1cec22c
Hleb <hlebchaba@gmail.com>
2020-11-27 11:09:11 +0200

    ImageSlider

--- Fishing/FishingDetailViewController.swift
+++ Fishing/FishingDetailViewController.swift
@@ -14,9 +14,14 @@ class FishingDetailViewController: UIViewController {
     @IBOutlet private weak var fishingName: UITextField!
     @IBOutlet private weak var fishingNote: UITextView!
     @IBOutlet private weak var fishingImage: UIImageView!
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    
     var fish: FishingInfo?
+    
     var onDataAdded: ((FishingInfo) -> Void)?
-
+    
+    var images: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,7 +29,7 @@ class FishingDetailViewController: UIViewController {
         fishingNote.text = fish?.notes
         guard let data = fish?.photo else {return}
         guard let loadedImage = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [Data] else {return}
-        fishingImage.image = UIImage(data: loadedImage.first ?? Data())
+        images = loadedImage.compactMap({ UIImage(data: $0) })
        // fishingImage.image = fish?.photo.flatMap { UIImage(data: $0) }
         fishingData.text = fish?.timeData
             
@@ -52,5 +57,20 @@ class FishingDetailViewController: UIViewController {
         try? context.save()
         self.onDataAdded?(info)
         self.navigationController?.popViewController(animated: true)
+    }
+}
+
+extension FishingDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if let vccc = cell.viewWithTag(111) as? UIImageView {
+            vccc.image = images[indexPath.row] 
+        }
+        return cell
     }
 }
